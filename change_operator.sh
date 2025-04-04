a8694c640a3a2c6d316cdad0afeaf0a4c71d2002
ReBullet <rebullet@ik.me>
2025-04-04 05:51:48 +0200

    Update change_operator.sh

--- change_operator.sh
+++ change_operator.sh
@@ -4,8 +4,8 @@
 while true; do
     resetprop gsm.operator.iso-country kz
     resetprop gsm.sim.operator.iso-country kz
-    resetprop gsm.operator.numeric 40102
-    resetprop gsm.sim.operator.numeric 40102
+    resetprop gsm.operator.numeric 40101
+    resetprop gsm.sim.operator.numeric 40101
     resetprop gsm.operator.alpha "Beeline"
     resetprop gsm.sim.operator.alpha "Beeline"
     sleep 10
