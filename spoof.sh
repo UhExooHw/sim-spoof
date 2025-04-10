429fb28e06e220f623f557a2b4ca0948a4bb5544
ReBullet <admin@rebullet.ch>
2025-04-10 21:04:22 +0200

    Update spoof.sh

--- spoof.sh
+++ spoof.sh
@@ -54,7 +54,7 @@ while true; do
   echo "  ${BOLD}[8]${RESET} Salt (Switzerland)"
   echo "  ${BOLD}[9]${RESET} Turkcell (Turkey)"
   echo "  ${BOLD}[9]${RESET} Telia (Finland)"
-  echo -n "${BOLD}Enter number (1-9): ${RESET}"
+  echo -n "${BOLD}Enter number (1-10): ${RESET}"
   read OPERATOR_CHOICE
   case "$OPERATOR_CHOICE" in
     1)
