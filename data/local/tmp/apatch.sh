6afeb259a6d84f9740de48578bf3d6e0694d9703
ReBullet <admin@rebullet.ch>
2025-09-13 01:33:10 +0400

    Update apatch.sh

--- data/local/tmp/apatch.sh
+++ data/local/tmp/apatch.sh
@@ -157,16 +157,20 @@ done
 /data/adb/ap/bin/resetprop -n gsm.sim.operator.iso-country "$ISO,$ISO"
 /data/adb/ap/bin/resetprop -n gsm.operator.numeric "$MCCMNC,$MCCMNC"
 /data/adb/ap/bin/resetprop -n gsm.sim.operator.numeric "$MCCMNC,$MCCMNC"
+/data/adb/ap/bin/resetprop -n ro.cdma.home.operator.numeric "$MCCMNC,$MCCMNC"
 /data/adb/ap/bin/resetprop -n ril.mcc.mnc0 "$MCCMNC,$MCCMNC"
 /data/adb/ap/bin/resetprop -n ril.mcc.mnc1 "$MCCMNC,$MCCMNC"
 /data/adb/ap/bin/resetprop -n debug.tracing.mcc "$MCC"
 /data/adb/ap/bin/resetprop -n debug.tracing.mnc "$MNC"
 /data/adb/ap/bin/resetprop -n gsm.operator.alpha "$OPERATOR,$OPERATOR"
+/data/adb/ap/bin/resetprop -n ro.cdma.home.operator.alpha "$OPERATOR,$OPERATOR"
 /data/adb/ap/bin/resetprop -n gsm.sim.operator.alpha "$OPERATOR,$OPERATOR"
+/data/adb/ap/bin/resetprop -n ro.carrier.name "$OPERATOR"
 /data/adb/ap/bin/resetprop -n persist.sys.timezone "$TZ"
 /data/adb/ap/bin/resetprop -n gsm.operator.isroaming "false,false"
 /data/adb/ap/bin/resetprop -n sys.wifitracing.started "0"
 /data/adb/ap/bin/resetprop -n persist.vendor.wifienhancelog "0"
+/data/adb/ap/bin/resetprop -n ro.com.android.dataroaming "0"
 /data/adb/ap/bin/resetprop -n persist.vendor.radio.imei  "$IMEI1"
 /data/adb/ap/bin/resetprop -n persist.vendor.radio.imei1 "$IMEI1"
 /data/adb/ap/bin/resetprop -n persist.vendor.radio.imei2 "$IMEI2"
@@ -197,7 +201,7 @@ done
 
 while true; do
     /data/adb/ap/bin/busybox sh /data/adb/service.d/SIM-Spoof.sh
-    sleep 5
+    sleep 10
 done
 EOF
 
