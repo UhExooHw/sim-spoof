c2e1a4743de0513d29990b05342c8e231a0815de
ReBullet <admin@rebullet.ch>
2025-09-12 13:30:16 +0400

    Update ksu.sh

--- data/local/tmp/ksu.sh
+++ data/local/tmp/ksu.sh
@@ -9,7 +9,7 @@
 /data/adb/ksu/bin/busybox echo "[•] Checking environment..."
 
 /data/adb/ksu/bin/busybox test ! -d /data/adb/service.d && /data/adb/ksu/bin/busybox echo "[×] Root solution KernelSU not installed. Exiting." && exit 1
-/data/adb/ksu/bin/busybox test ! -d /data/adb/modules/systemless-hosts-KernelSU-module/system/etc && /data/adb/ksu/bin/busybox echo "[×] Systemless hosts module directory not found. Exiting." && exit 1
+/data/adb/ksu/bin/busybox test ! -d /data/adb/modules/bindhosts/system/etc && /data/adb/ksu/bin/busybox echo "[×] Bindhosts hosts not installed. Exiting." && exit 1
 
 NEW_ANDROID_ID=$(/data/adb/ksu/bin/busybox hexdump -n8 -ve '/1 "%02x"' /dev/urandom)
 BBR_ALGORITHM=""
@@ -130,7 +130,7 @@ while true; do
 done
 
 /data/adb/ksu/bin/busybox echo "[•] Downloading hosts file..."
-/data/adb/ksu/bin/busybox wget -O /data/adb/modules/systemless-hosts-KernelSU-module/system/etc/hosts https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts || { /data/adb/ksu/bin/busybox echo "[×] Failed to download hosts file."; exit 1; }
+/data/adb/ksu/bin/busybox wget -O /data/adb/modules/bindhosts/system/etc/hosts https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts || { /data/adb/ksu/bin/busybox echo "[×] Failed to download hosts file."; exit 1; }
 
 RBI1=$(/data/adb/ksu/bin/busybox printf "%02d" $((RANDOM % 100)))
 TAC1=$(/data/adb/ksu/bin/busybox printf "%06d" $((RANDOM % 1000000)))
