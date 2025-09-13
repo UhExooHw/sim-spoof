1b7a36f09c6f985f6f95999505a7600d02e7936d
ReBullet <admin@rebullet.ch>
2025-09-13 19:44:36 +0400

    Update ksu.sh

--- data/local/tmp/ksu.sh
+++ data/local/tmp/ksu.sh
@@ -8,8 +8,8 @@
 
 /data/adb/ksu/bin/busybox echo "[•] Checking environment..."
 
-/data/adb/ksu/bin/busybox test ! -d /data/adb/service.d && /data/adb/ksu/bin/busybox echo "[×] Root solution KernelSU not installed. Exiting." && exit 1
-/data/adb/ksu/bin/busybox test ! -d /data/adb/modules/systemless-hosts/system/etc && /data/adb/ksu/bin/busybox echo "[×] Systemless-hosts not installed. Exiting." && exit 1
+/data/adb/ksu/bin/busybox test ! -d /data/adb/service.d && /data/adb/ksu/bin/busybox echo "[×] Root solution not installed. Exiting." && exit 1
+/data/adb/ksu/bin/busybox test ! -d /data/adb/modules/systemless-apns && /data/adb/ksu/bin/busybox echo "[×] Systemless-apns not installed. Exiting." && exit 1
 
 CHARS="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
 SERIAL_NO=""
@@ -31,88 +31,18 @@ fi
 /data/adb/ksu/bin/busybox which ip6tables >/dev/null 2>&1 || { /data/adb/ksu/bin/busybox echo "[×] ip6tables not found. Exiting."; exit 1; }
 /data/adb/ksu/bin/busybox echo "[✓] Environment OK."
 
-while true; do
-    /data/adb/ksu/bin/busybox echo "Select Mobile Operator:"
-    /data/adb/ksu/bin/busybox echo "  [1] Beeline  [2] MTS  [3] Tele2   [4] Megafon"
-    /data/adb/ksu/bin/busybox echo "  [5] Yota  [6] Velcom  [7] Life:)   [8] Salt"
-    /data/adb/ksu/bin/busybox echo "  [9] Turkcell  [10] Tele Finland  [11] Telekom   [12] KPN"
-    /data/adb/ksu/bin/busybox echo "  [13] Airtel  [14] Kyivstar  [15] Test   [16] Irancell"
-    /data/adb/ksu/bin/busybox echo "  [17] Custom  [0] Exit"
-    /data/adb/ksu/bin/busybox echo -n "Enter number (0-17): "
-    read OPERATOR_CHOICE
-    case "$OPERATOR_CHOICE" in
-        0) /data/adb/ksu/bin/busybox echo "Exiting..."; exit 0 ;;
-        1)
-            while true; do
-                /data/adb/ksu/bin/busybox echo "Select Country:"
-                /data/adb/ksu/bin/busybox echo "  [1] Uzbekistan  [2] Kazakhstan  [3] Russia  [0] Back"
-                /data/adb/ksu/bin/busybox echo -n "Enter number (0-3): "
-                read COUNTRY_CHOICE
-                case "$COUNTRY_CHOICE" in
-                    0) break ;;
-                    1) MCCMNC="43404" MCC="434" MNC="04" ISO="uz" TZ="Asia/Tashkent" OPERATOR="Beeline"; break 2 ;;
-                    2) MCCMNC="40101" MCC="401" MNC="01" ISO="kz" TZ="Asia/Almaty" OPERATOR="Beeline"; break 2 ;;
-                    3) MCCMNC="25099" MCC="250" MNC="99" ISO="ru" TZ="Europe/Moscow" OPERATOR="Beeline"; break 2 ;;
-                    *) /data/adb/ksu/bin/busybox echo "[!] Invalid option." ;;
-                esac
-            done
-            ;;
-        2)
-            while true; do
-                /data/adb/ksu/bin/busybox echo "Select Country:"
-                /data/adb/ksu/bin/busybox echo "  [1] Belarus  [2] Russia  [0] Back"
-                /data/adb/ksu/bin/busybox echo -n "Enter number (0-2): "
-                read COUNTRY_CHOICE
-                case "$COUNTRY_CHOICE" in
-                    0) break ;;
-                    1) MCCMNC="25702" MCC="257" MNC="02" ISO="by" TZ="Europe/Minsk" OPERATOR="MTS"; break 2 ;;
-                    2) MCCMNC="25001" MCC="250" MNC="01" ISO="ru" TZ="Europe/Moscow" OPERATOR="MTS"; break 2 ;;
-                    *) /data/adb/ksu/bin/busybox echo "[!] Invalid option." ;;
-                esac
-            done
-            ;;
-        3)
-            while true; do
-                /data/adb/ksu/bin/busybox echo "Select Country:"
-                /data/adb/ksu/bin/busybox echo "  [1] Russia  [2] Latvia  [3] Sweden  [4] Estonia  [0] Back"
-                /data/adb/ksu/bin/busybox echo -n "Enter number (0-4): "
-                read COUNTRY_CHOICE
-                case "$COUNTRY_CHOICE" in
-                    0) break ;;
-                    1) MCCMNC="25020" MCC="250" MNC="20" ISO="ru" TZ="Europe/Moscow" OPERATOR="Tele2 RU"; break 2 ;;
-                    2) MCCMNC="24702" MCC="247" MNC="02" ISO="lv" TZ="Europe/Riga" OPERATOR="Tele2"; break 2 ;;
-                    3) MCCMNC="24007" MCC="240" MNC="07" ISO="se" TZ="Europe/Stockholm" OPERATOR="Tele2"; break 2 ;;
-                    4) MCCMNC="24803" MCC="248" MNC="03" ISO="ee" TZ="Europe/Tallinn" OPERATOR="Tele2"; break 2 ;;
-                    *) /data/adb/ksu/bin/busybox echo "[!] Invalid option." ;;
-                esac
-            done
-            ;;
-        4) MCCMNC="25002" MCC="250" MNC="02" ISO="ru" TZ="Europe/Moscow" OPERATOR="Megafon"; break ;;
-        5) MCCMNC="25011" MCC="250" MNC="11" ISO="ru" TZ="Europe/Moscow" OPERATOR="Yota"; break ;;
-        6) MCCMNC="25701" MCC="257" MNC="01" ISO="by" TZ="Europe/Minsk" OPERATOR="Velcom"; break ;;
-        7) MCCMNC="25704" MCC="257" MNC="04" ISO="by" TZ="Europe/Minsk" OPERATOR="life:)"; break ;;
-        8) MCCMNC="22803" MCC="228" MNC="03" ISO="ch" TZ="Europe/Zurich" OPERATOR="Salt"; break ;;
-        9) MCCMNC="28601" MCC="286" MNC="01" ISO="tr" TZ="Europe/Istanbul" OPERATOR="Turkcell"; break ;;
-        10) MCCMNC="24491" MCC="244" MNC="91" ISO="fi" TZ="Europe/Helsinki" OPERATOR="Tele Finland"; break ;;
-        11) MCCMNC="26201" MCC="262" MNC="01" ISO="de" TZ="Europe/Berlin" OPERATOR="Telekom"; break ;;
-        12) MCCMNC="20408" MCC="204" MNC="08" ISO="nl" TZ="Europe/Amsterdam" OPERATOR="KPN"; break ;;
-        13) MCCMNC="40402" MCC="404" MNC="02" ISO="in" TZ="Asia/Kolkata" OPERATOR="Airtel"; break ;;
-        14) MCCMNC="25503" MCC="255" MNC="03" ISO="ua" TZ="Europe/Kyiv" OPERATOR="Kyivstar"; break ;;
-        15) MCCMNC="00101" MCC="001" MNC="01" ISO="aq" TZ="Antarctica/Vostok" OPERATOR="Test"; break ;;
-        16) MCCMNC="43235" MCC="432" MNC="35" ISO="ir" TZ="Asia/Tehran" OPERATOR="Irancell"; break ;;
-        17)
-            /data/adb/ksu/bin/busybox echo "Manual Custom Input:"
-            /data/adb/ksu/bin/busybox echo -n "MCCMNC: "; read MCCMNC
-            /data/adb/ksu/bin/busybox echo -n "MCC: "; read MCC
-            /data/adb/ksu/bin/busybox echo -n "MNC: "; read MNC
-            /data/adb/ksu/bin/busybox echo -n "ISO: "; read ISO
-            /data/adb/ksu/bin/busybox echo -n "TimeZone: "; read TZ
-            /data/adb/ksu/bin/busybox echo -n "Operator: "; read OPERATOR
-            break
-            ;;
-        *) /data/adb/ksu/bin/busybox echo "[!] Invalid option." ;;
-    esac
-done
+MCCMNC="90114"
+MCC="901"
+MNC="14"
+ISO="SC"
+OPERATOR="ReBullet Internet"
+/data/adb/ksu/bin/busybox echo "Manual Timezone Input:"
+/data/adb/ksu/bin/busybox echo -n "Enter Timezone (e.g., Europe/Moscow): "
+read TZ
+if [[ ! "$TZ" =~ "/" ]]; then
+    /data/adb/ksu/bin/busybox echo "[×] Invalid timezone format. Must contain a forward slash (e.g., Europe/Moscow)."
+    exit 1
+fi
 
 while true; do
     /data/adb/ksu/bin/busybox echo "Choose DNS Provider:"
@@ -136,7 +66,7 @@ while true; do
 done
 
 /data/adb/ksu/bin/busybox echo "[•] Downloading hosts file..."
-/data/adb/ksu/bin/busybox wget -O /data/adb/modules/systemless-hosts/system/etc/hosts https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts
+/data/adb/ksu/bin/busybox wget -O /data/adb/modules/systemless-apns/system/etc/hosts https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts
 
 rm /data/system/users/0/settings_ssaid.xml
 
@@ -259,14 +189,14 @@ iptables -t nat -C OUTPUT -p udp --dport 53 -j DNAT --to-destination \$DNS:53 2>
 /data/adb/ksu/bin/resetprop -n net.rmnet3.dns2 \$DNS
 /data/adb/ksu/bin/resetprop -n net.pdpbr1.dns1 \$DNS
 /data/adb/ksu/bin/resetprop -n net.pdpbr1.dns2 \$DNS
-/data/adb/ksu/bin/resetprop -n wlan0.dns1 \$DNS
-/data/adb/ksu/bin/resetprop -n wlan0.dns2 \$DNS
-/data/adb/ksu/bin/resetprop -n wlan1.dns1 \$DNS
-/data/adb/ksu/bin/resetprop -n wlan1.dns2 \$DNS
-/data/adb/ksu/bin/resetprop -n wlan2.dns1 \$DNS
-/data/adb/ksu/bin/resetprop -n wlan2.dns2 \$DNS
-/data/adb/ksu/bin/resetprop -n wlan3.dns1 \$DNS
-/data/adb/ksu/bin/resetprop -n wlan3.dns2 \$DNS
+/data/adb/ksu/bin/resetprop -n net.wlan0.dns1 \$DNS
+/data/adb/ksu/bin/resetprop -n net.wlan0.dns2 \$DNS
+/data/adb/ksu/bin/resetprop -n net.wlan1.dns1 \$DNS
+/data/adb/ksu/bin/resetprop -n net.wlan1.dns2 \$DNS
+/data/adb/ksu/bin/resetprop -n net.wlan2.dns1 \$DNS
+/data/adb/ksu/bin/resetprop -n net.wlan2.dns2 \$DNS
+/data/adb/ksu/bin/resetprop -n net.wlan3.dns1 \$DNS
+/data/adb/ksu/bin/resetprop -n net.wlan3.dns2 \$DNS
 EOF
 
 /data/adb/ksu/bin/busybox chmod +x /data/adb/service.d/SIM-*.sh
